4b13ffb6ecc80a73621b6d93e34400de271b5c75
sonhm-code <sonhm@kakao.com>
2021-03-24 21:33:39 +0900

    add update and delete

--- env/ktranslate/posts/models.py
+++ env/ktranslate/posts/models.py
@@ -5,7 +5,6 @@ from django.db import models
 class Post(models.Model):
     author = models.CharField(max_length=100)
     request = models.TextField()
-    response = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
--- env/ktranslate/posts/views.py
+++ env/ktranslate/posts/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Post
+from django.utils import timezone
 
 # Create your views here.
 
@@ -20,3 +21,41 @@ def detail(request,post_id):
     }
 
     return render(request, 'posts/detail.html', context)
+
+
+def new(request):
+    return render(request, 'posts/new.html')
+
+
+def create(request):
+    author = request.POST.get('author')
+    request = request.POST.get('request')
+    post = Post(author=author, request=request, created_at=timezone.now())
+    post.save()
+
+    return redirect('posts:detail', post_id = post.id)
+
+
+def edit(request,post_id):
+    post = Post.objects.get(id = post_id)
+    context = {
+        "post" : post
+    }
+
+    return render(request, 'posts/edit.html', context)
+
+
+def update(request, post_id):
+    post = Post.objects.get(id = post_id)
+    post.author = request.POST.get('author')
+    post.request = request.POST.get('request')
+    post.save()
+
+    return redirect('posts:detail', post_id = post.id)
+
+
+def delete(request, post_id):
+    post = Post.objects.get(id = post_id)
+    post.delete()
+
+    return redirect('posts:index')
