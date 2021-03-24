86bab0844df2671f49304bee87be766f3fab4be7
sonhm-code <sonhm@kakao.com>
2021-03-24 17:44:35 +0900

    add detail page

--- env/ktranslate/posts/models.py
+++ env/ktranslate/posts/models.py
@@ -6,7 +6,7 @@ class Post(models.Model):
     author = models.CharField(max_length=100)
     request = models.TextField()
     response = models.TextField()
-    created_at = models.DateTimeField
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f'{self.author}: {self.request}'
--- env/ktranslate/posts/views.py
+++ env/ktranslate/posts/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
 
 def index(request):
-    context ={
-        'post' : 'sonhm'
+    posts = Post.objects.all()
+    context = {
+        'posts' : posts
     }
 
     return render(request,'posts/index.html',context)
+
+
+def detail(request,post_id):
+    post = Post.objects.get(id = post_id)
+    context = {
+        'post' : post
+    }
+
+    return render(request, 'posts/detail.html', context)
