e0037814017f1a141760a6dfb32d8cbc28b19659
sonhm-code <sonhm@kakao.com>
2021-03-27 16:53:46 +0900

    fix posts model.py

--- env/ktranslate/posts/models.py
+++ env/ktranslate/posts/models.py
@@ -14,13 +14,3 @@ class Post(models.Model):
         else:
             return f'{self.request}'
 
-
-class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
-    response = models.CharField(max_length=300)
-    liked_users = models.ManyToManyField(User, related_name='liked_comments')
-    created_at = models.DateTimeField(auto_now_add=True)
-    
-    def __str__(self):
-        return f'{self.user}: {self.response}'
